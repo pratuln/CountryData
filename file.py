e21ba9cc3f27bf94dcadcaadc8cd7f923b292950
pratuln <pratulneupane@gmail.com>
2023-06-12 15:04:51 -0400

    Completed everything, onto tableau

--- file.py
+++ file.py
@@ -7,13 +7,14 @@ df = pd.read_csv('CPI.csv', header=4)
 # drop all the columns that we don't need
 df = df.drop(columns=['Country Code', 'Indicator Name', 'Indicator Code'])
 # drop the years 1960-2017
-df = df.drop(df.iloc[:, 1:59], axis=1)
+df = df.drop(df.iloc[:, 1:58], axis=1)
 
 df = df.loc[df['Country Name'].isin(['United Arab Emirates', 'United States', 'United Kingdom', 'China', 'Japan', 'India', 'Germany', 'France', 'Canada', 'Brazil', 'Colombia', 'Poland'])]
 # print(df)
 
 df = df.set_index('Country Name')
 df = df.transpose()
+df = df.drop(df.index[5])
 # sort alphabetically
 df = df.sort_index()
 
@@ -23,16 +24,17 @@ df = df.melt(id_vars=['index'], value_vars=['United Arab Emirates', 'United Stat
 df = df.rename(columns={'index': 'Year', 'variable': 'Country Name', 'value': 'CPI'})
 df = df.sort_values(by=['Year', 'Country Name'])
 df = df.reset_index(drop=True)
-print(df)
+# print(df)
 # print("GDP:\n")
 
 # make a pandas dataframe for the GDP and do the same thing as above
 df2 = pd.read_csv('GDP.csv', header=2)
 df2 = df2.drop(columns=['Country Code', 'Indicator Name', 'Indicator Code'])
-df2 = df2.drop(df2.iloc[:, 1:59], axis=1)
+df2 = df2.drop(df2.iloc[:, 1:58], axis=1)
 df2 = df2.loc[df2['Country Name'].isin(['United Arab Emirates', 'United States', 'United Kingdom', 'China', 'Japan', 'India', 'Germany', 'France', 'Canada', 'Brazil', 'Colombia', 'Poland'])]
 df2 = df2.set_index('Country Name')
 df2 = df2.transpose()
+df2 = df2.drop(df2.index[6])
 df2 = df2.drop(df2.index[5])
 df2 = df2.sort_index()
 df2 = df2.reset_index()
@@ -40,14 +42,15 @@ df2 = df2.melt(id_vars=['index'], value_vars=['United Arab Emirates', 'United St
 df2 = df2.rename(columns={'index': 'Year', 'variable': 'Country Name', 'value': 'GDP'})
 df2 = df2.sort_values(by=['Year', 'Country Name'])
 df2 = df2.reset_index(drop=True)
-print(df2)
+# print(df2)
 
 df3 = pd.read_csv('Inflation.csv', header=2)
 df3 = df3.drop(columns=['Country Code', 'Indicator Name', 'Indicator Code'])
-df3 = df3.drop(df3.iloc[:, 1:59], axis=1)
+df3 = df3.drop(df3.iloc[:, 1:58], axis=1)
 df3 = df3.loc[df3['Country Name'].isin(['United Arab Emirates', 'United States', 'United Kingdom', 'China', 'Japan', 'India', 'Germany', 'France', 'Canada', 'Brazil', 'Colombia', 'Poland'])]
 df3 = df3.set_index('Country Name')
 df3 = df3.transpose()
+df3 = df3.drop(df3.index[6])
 df3 = df3.drop(df3.index[5])
 df3 = df3.sort_index()
 df3 = df3.reset_index()
@@ -55,7 +58,7 @@ df3 = df3.melt(id_vars=['index'], value_vars=['United Arab Emirates', 'United St
 df3 = df3.rename(columns={'index': 'Year', 'variable': 'Country Name', 'value': 'Inflation'})
 df3 = df3.sort_values(by=['Year', 'Country Name'])
 df3 = df3.reset_index(drop=True)
-print(df3)
+# print(df3)
 
 # # combine the dataframes into one dataframe, making sure to match the years and countries
 # CPI, GDP, Inflation each get their own column
